3a38db04735483b4d16c5ee3ed99e070c9622eaf
reftch <j.developer@hotmail.com>
2024-05-17 21:13:11 +0200

    Added neovim configuration

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+-- turn off swapfile
+opt.swapfile = false
+
+opt.title = true
+
+-- virtual edit
+-- opt.ve = "all"
